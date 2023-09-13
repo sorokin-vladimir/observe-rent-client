24206a792d0d03333d6ba88f5376ee730626f946
Vladimir Sorokin <v.sorokin@hey.com>
2023-09-13 14:04:14 +0300

    added nanostores/logger

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,6 +1,26 @@
+import { logger } from '@nanostores/logger'
+import { dev, browser,  } from "$app/environment";
+import { user } from './user';
+import { housings } from './housing';
+import { fields } from './fields';
+import { counters } from './counters';
+import { ui } from './ui';
+
 export * from './housing';
 export * from './user';
 export * from './fields';
 export * from './ui';
 export * from './table_data';
 export * from './counters';
+
+
+
+if (dev && browser) {
+  /* let destroy =  */logger({
+    Housings: housings,
+    Fields: fields,
+    Counters: counters,
+    User: user,
+    UI: ui,
+  });
+}
